5a69c33a3858c8616f1c030a7e8f958e96629e02
Hau <haajutran@gmail.com>
2019-06-18 21:56:00 +0700

    Updates

---
+++ precache-manifest.4edeba0828dc028891d5b1c5c1d59ecf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b8a890e9df455b0e363586fcc902a9",
+    "url": "/POS-API/index.html"
+  },
+  {
+    "revision": "cebd3061b17deeed6036",
+    "url": "/POS-API/static/css/2.feff5396.chunk.css"
+  },
+  {
+    "revision": "500e2c37d7fcb2b6fec4",
+    "url": "/POS-API/static/css/main.e3a649ef.chunk.css"
+  },
+  {
+    "revision": "cebd3061b17deeed6036",
+    "url": "/POS-API/static/js/2.d85b875f.chunk.js"
+  },
+  {
+    "revision": "500e2c37d7fcb2b6fec4",
+    "url": "/POS-API/static/js/main.d9c3f382.chunk.js"
+  },
+  {
+    "revision": "2039f5ef6c429ada79dd",
+    "url": "/POS-API/static/js/runtime~main.de16c067.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/POS-API/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/POS-API/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/POS-API/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/POS-API/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/POS-API/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "3684fb1af8fc4e944fa49aa9c1c6748b",
+    "url": "/POS-API/static/media/logo.3684fb1a.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/POS-API/static/media/logo.5d5d9eef.svg"
+  }
+]);
